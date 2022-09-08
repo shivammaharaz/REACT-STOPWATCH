50b4cc58d818ec5913b7296e2c87182101051dc7
Shivam Kumar <shivamraj6136@gmail.com>
2022-09-08 10:46:47 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [time,setTime]=useState(0)
+  const [timeOn,setTimeOn]=useState(false);
+
+  useEffect(()=>{
+    let interval =null;
+    if(timeOn){
+    interval =setInterval(()=>
+    {
+      setTime(prevTime=> prevTime+10)
+    },10)
+  }
+  else{
+    clearInterval(interval);
+  }
+
+  return ()=>clearInterval(interval)
+  })
+
+  const reset=()=>{
+    setTime(0)
+    setTimeOn(false)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='watch-face'>
+        <span>{('0'+Math.floor((time/60000))).slice(-2)}  :</span>
+        <span>{("0"+Math.floor((time/1000))).slice(-2)}  :</span>
+        <span>{("0"+( (time/10)%100)).slice(-2)}</span>
+        {/* :{((time/10) % 100)} */}
+      </div>
+      <div className='btn-div'>
+      <button className='btn' onClick={()=>setTimeOn(!timeOn)}>{timeOn?"pause":"start"}</button>
+      {/* <button onClick={()=>setTimeOn(false)}>pause</button> */}
+      <button className='btn' onClick={reset}>reset</button></div>
     </div>
   );
 }
 
 export default App;
+
+// https://stopswatch.netlify.app/
